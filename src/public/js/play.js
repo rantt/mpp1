136903b1e99538885e94d5cd2dcc5513af737ca4
Dave Canam <david.canam@gmail.com>
2016-03-19 21:09:36 -0300

    add touch controls

--- src/public/js/play.js
+++ src/public/js/play.js
@@ -18,6 +18,10 @@ var aKey;
 var sKey;
 var dKey;
 var cursors;
+var leftArrow;
+var rightArrow;
+var upArrow;
+var downArrow;
 var score = 0;
 var FLOOR,WALL;
 var actors = {};
@@ -154,31 +158,29 @@ Game.Play.prototype = {
         this.body.velocity.x = 0;
         this.body.velocity.y = 0;
       }else{
-          // this.socket.emit('x',this.x);
-          // this.socket.emit('y',this.y);
         //Don't move when the dialogue box is visible
-        if (cursors.left.isDown || aKey.isDown) {
+        if (cursors.left.isDown || aKey.isDown || leftArrow) {
           // SOCKET.emit('player',pos);
           this.body.velocity.x = -speed;
           this.direction = 'left';
           this.animations.play('left');
           SOCKET.emit('player',{x: this.x, y: this.y, moving: true, direction: 'left'});
         }
-        else if (cursors.right.isDown || dKey.isDown) {
+        else if (cursors.right.isDown || dKey.isDown || rightArrow) {
           // SOCKET.emit('player',pos);
           this.body.velocity.x = speed;
           this.direction = 'right';
           this.animations.play('right');
           SOCKET.emit('player',{x: this.x, y: this.y, moving: true, direction: 'right'});
         }
-        else if (cursors.up.isDown || wKey.isDown) {
+        else if (cursors.up.isDown || wKey.isDown || upArrow) {
           // SOCKET.emit('player',pos);
           this.body.velocity.y = -speed;
           this.direction = 'up';
           this.animations.play('up');
           SOCKET.emit('player',{x: this.x, y: this.y, moving: true, direction: 'up'});
         }
-        else if (cursors.down.isDown || sKey.isDown) {
+        else if (cursors.down.isDown || sKey.isDown || downArrow) {
           // SOCKET.emit('player',pos);
           this.body.velocity.y = speed;
           this.direction = 'down';
@@ -188,19 +190,19 @@ Game.Play.prototype = {
         else {
           if (this.direction === 'up') {
             this.frame = 1;
-            // SOCKET.emit('player',{x: this.x, y: this.y, moving: false, direction: 'up'});
+            SOCKET.emit('player',{x: this.x, y: this.y, moving: false, direction: 'up'});
           }
           else if (this.direction === 'down') {
             this.frame = 0;
-            // SOCKET.emit('player',{x: this.x, y: this.y, moving: false, direction: 'down'});
+            SOCKET.emit('player',{x: this.x, y: this.y, moving: false, direction: 'down'});
           }
           else if (this.direction === 'right') {
             this.frame = 2;
-            // SOCKET.emit('player',{x: this.x, y: this.y, moving: false, direction: 'right'});
+            SOCKET.emit('player',{x: this.x, y: this.y, moving: false, direction: 'right'});
           }
           else if (this.direction === 'left') {
             this.frame = 3;
-            // SOCKET.emit('player',{x: this.x, y: this.y, moving: false, direction: 'left'});
+            SOCKET.emit('player',{x: this.x, y: this.y, moving: false, direction: 'left'});
           }
           this.animations.stop();
         }
@@ -219,11 +221,104 @@ Game.Play.prototype = {
     dKey = this.game.input.keyboard.addKey(Phaser.Keyboard.D);
     // muteKey = game.input.keyboard.addKey(Phaser.Keyboard.M);
 
+
+    this.loadTouchControls();
+
+
+
+
     //Create Twitter button as invisible, show during win condition to post highscore
     this.twitterButton = this.game.add.button(this.game.world.centerX, this.game.world.centerY + 200,'twitter', this.twitter, this);
     this.twitterButton.anchor.set(0.5);
     this.twitterButton.visible = false;
   },
+  loadTouchControls: function() {
+    if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+      //Draw Arrow w/ Bitmap Data
+      var bmdsize = 80;
+      this.arrowbmd = this.game.add.bitmapData(bmdsize,bmdsize);
+      this.arrowbmd.ctx.clearRect(0,0,bmdsize,bmdsize);
+      this.arrowbmd.ctx.strokeStyle = 'white';
+      this.arrowbmd.ctx.lineWidth = 2;
+      this.arrowbmd.ctx.fill();
+      this.arrowbmd.ctx.beginPath();
+      this.arrowbmd.ctx.moveTo(bmdsize*1/2,0);
+      this.arrowbmd.ctx.lineTo(0,bmdsize*1/2);
+      this.arrowbmd.ctx.lineTo(bmdsize*1/4,bmdsize*1/2);
+      this.arrowbmd.ctx.lineTo(bmdsize*1/4,bmdsize);
+      this.arrowbmd.ctx.lineTo(bmdsize*3/4,bmdsize);
+      this.arrowbmd.ctx.lineTo(bmdsize*3/4,bmdsize*1/2);
+      this.arrowbmd.ctx.lineTo(bmdsize,bmdsize*1/2);
+      this.arrowbmd.ctx.fill();
+
+      //Add Touch Controls for mobile
+      //Up Arrow
+      this.upArrow = this.game.add.sprite(140, Game.h - 160, this.arrowbmd);
+      this.upArrow.tint = 0xdcdcdc;
+      this.upArrow.alpha = 0.5;
+      this.upArrow.anchor.setTo(0.5, 0.5);
+      this.upArrow.inputEnabled = true;
+      this.upArrow.fixedToCamera = true;
+      this.upArrow.events.onInputDown.add(function() {
+        upArrow = true;
+      },this);
+      this.upArrow.events.onInputUp.add(function() {
+        upArrow = false;
+      },this);
+      // this.upArrow.visible = false;
+
+      //Up Down
+      this.downArrow = this.game.add.sprite(140, Game.h - 40, this.arrowbmd);
+      this.downArrow.tint = 0xdcdcdc;
+      this.downArrow.alpha = 0.5;
+      this.downArrow.anchor.setTo(0.5, 0.5);
+      this.downArrow.inputEnabled = true;
+      this.downArrow.angle = 180;
+      this.downArrow.fixedToCamera = true;
+      this.downArrow.events.onInputDown.add(function() {
+        downArrow = true;
+      },this);
+      this.downArrow.events.onInputUp.add(function() {
+        downArrow = false;
+      },this);
+      // this.downArrow.visible = false;
+
+      //Up Left 
+      this.leftArrow = this.game.add.sprite(60, Game.h - 100, this.arrowbmd);
+      this.leftArrow.tint = 0xdcdcdc;
+      this.leftArrow.alpha = 0.5;
+      this.leftArrow.anchor.setTo(0.5, 0.5);
+      this.leftArrow.inputEnabled = true;
+      this.leftArrow.fixedToCamera = true;
+      this.leftArrow.angle = -90;
+      this.leftArrow.events.onInputDown.add(function() {
+        leftArrow = true;
+      },this);
+      this.leftArrow.events.onInputUp.add(function() {
+        leftArrow = false;
+      },this);
+      // this.leftArrow.visible = false;
+
+      //Up Right 
+      this.rightArrow = this.game.add.sprite(220, Game.h - 100, this.arrowbmd);
+      this.rightArrow.tint = 0xdcdcdc;
+      this.rightArrow.alpha = 0.5;
+      this.rightArrow.anchor.setTo(0.5, 0.5);
+      this.rightArrow.inputEnabled = true;
+      this.rightArrow.angle = 90;
+      this.rightArrow.fixedToCamera = true;
+      this.rightArrow.events.onInputDown.add(function() {
+        rightArrow = true;
+      },this);
+      this.rightArrow.events.onInputUp.add(function() {
+        rightArrow = false;
+      },this);
+      // this.rightArrow.visible = false;
+
+    }
+
+  },
+
 	makeBox: function(x,y) {
 		var bmd = this.game.add.bitmapData(x, y);
 		bmd.ctx.beginPath();
